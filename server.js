0b0d93d58b071c124b358c7a9df601b6895e7a57
Lemon Sama <burrisc24@gmail.com>
2022-05-01 18:19:58 -0400

    schemas and seeds database files

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const db = require('./db/connection')
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+db.connect(err => {
+    if (err) throw err;
+    console.log('data base connected.')
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`)
+    });
+});
